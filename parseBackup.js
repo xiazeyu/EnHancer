f2044a94110258378f8cc3502c1452416c8b98da
xiazeyu <i@xiaze.pro>
2022-04-15 16:26:58 +0800

    it works!

--- parseBackup.js
+++ parseBackup.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const path = require("path");
-const fs = require("fs/promises");
+const fs = require("fs");
 const lookup = require("./src/api/lookup");
 const GalleryIdentifier = require("./src/api/gallery-identifier");
 const commonJson = require("./src/api/gallery-info/common-json");
 const request = require("./src/request");
 
-let useEx;
+const outputPrefix = 'output/';
 
 class Runner {
 
@@ -32,7 +32,7 @@ class Runner {
     
         // Authentication cookies
         if (this.useEx) {
-            const cookies = parseCookieString(exCookieString);
+            const cookies = this.parseCookieString(exCookieString);
             const requiredCookieNames = [ "ipb_member_id", "ipb_pass_hash", "igneous" ];
             for (const key in cookies) {
                 if (!Object.prototype.hasOwnProperty.call(cookies, key)) { continue; }
@@ -46,19 +46,28 @@ class Runner {
     }
     
     readCookieString() {
-        const exCookieFileName = "cookies.txt";
         try {
-            return fs.readFileSync(exCookieFileName, { encoding: "utf8" }).trim() || null;
+            return fs.readFileSync("cookies.txt", { encoding: "utf8" }).trim() || null;
         } catch (e) {
-            console.log(`Failed to read cookie file: ${exCookieFileName}`);
+            console.log(`Failed to read cookie file: cookies.txt ${e}`);
         }
         return null;
+    }
+
+    parseCookieString(cookieString) {
+        const re = /\s*([^=]*)=([^;]*)(?:;|$)/g;
+        let match;
+        const results = {};
+        while ((match = re.exec(cookieString)) !== null) {
+            results[match[1]] = match[2];
+        }
+        return results;
     }
 
     async getGalleryInfo(galleryIdentifier){
 
         const galleryInfos = await lookup.getGalleryInfos(this.site, this.cookieJar, [galleryIdentifier])
-        console.log(galleryInfos[0].info);
+        // console.log(galleryInfos[0].info);
         return galleryInfos[0].info;
 
     }
@@ -67,7 +76,7 @@ class Runner {
         const galleryInfo = await this.getGalleryInfo(galleryIdentifier);
         const json = commonJson.toCommonJson(galleryInfo);
         const jsonString = JSON.stringify(json, null, "  ");
-        console.log(`jsonString: ${jsonString}`);
+        // console.log(`jsonString: ${jsonString}`);
         return jsonString;
     }
 
@@ -75,7 +84,7 @@ class Runner {
 
 function generateGalleryIdentifier(tags){
 
-    console.log(tags);
+    // console.log(tags);
     const ehexp = /.*source:\s*e(?:x|-)hentai\.org\/g\/([0-9]*)\/([0-z]*)\/*.*/i;
 
     if(ehexp.test(tags)){
@@ -95,30 +104,53 @@ function generateGalleryIdentifier(tags){
     return galleryIdentifier;
 }
 
-async function main(){
-
-    const outputPrefix = 'output/';
-
-    const runner = new Runner();
-    const backupFileData = await fs.readFile("backup.json", "utf8");
-    const archivesObj = JSON.parse(backupFileData)["archives"];
-
-    const currentArchieve = archivesObj[0];
-
+async function generateGalleryInfoJson(runner, currentArchieve){
+    console.log(`now processing ${currentArchieve["arcid"]} ${currentArchieve["filename"]}.`);
     const galleryIdentifier = generateGalleryIdentifier(currentArchieve["tags"]);
-
-    await fs.mkdir(outputPrefix, {recursive: true});
-
     if (galleryIdentifier !== null){
-        const getGalleryInfoJson = await runner.getGalleryInfoJson(galleryIdentifier);
-        await fs.writeFile(path.resolve(outputPrefix, `${currentArchieve["filename"]}.json`), getGalleryInfoJson, "utf8");
+        try{
+            const getGalleryInfoJson = await runner.getGalleryInfoJson(galleryIdentifier);
+            await fs.promises.writeFile(path.resolve(outputPrefix, `${currentArchieve["filename"]}.json`), getGalleryInfoJson, "utf8");
+            return {'arcid': currentArchieve["arcid"], 'status': true}
+        }catch(e){
+            console.log(`${currentArchieve["arcid"]} ${currentArchieve["filename"]} failed ${e}`);
+            return {'arcid': currentArchieve["arcid"], 'status': false, 'reason': e}
+        }
     } else {
         console.log(`${currentArchieve["arcid"]} ${currentArchieve["filename"]} failed`);
+        return {'arcid': currentArchieve["arcid"], 'status': false, 'reason': 'GalleryIdentifier not exist.'}
     }
-
 }
 
+async function main(){
 
+    await fs.promises.mkdir(outputPrefix, {recursive: true});
+
+    const runner = new Runner();
+    const backupFileData = await fs.promises.readFile("backup.json", "utf8");
+    const archivesObj = JSON.parse(backupFileData)["archives"];
+
+    const final = {
+        'success': new Array(),
+        'fail': new Array(),
+    }
+
+    for (const currentArchieve of archivesObj) {
+        const result = await generateGalleryInfoJson(runner, currentArchieve);
+        await new Promise((r) => setTimeout(r, 1000))
+        if(result['status']){
+            final['success'].push(result);
+            console.log('success!!!');
+        }else{
+            final['fail'].push(result);
+            console.log('fail!!!');
+        }
+        console.log();
+        await fs.promises.writeFile('final.json', JSON.stringify(final), "utf8");
+    }
+
+
+}
 
 if (require.main === module) {
 	(async () => { await main(); })();
